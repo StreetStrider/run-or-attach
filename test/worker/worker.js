95a5ea90f47b101086513f1d4d79813503029ff0
StreetStrider <teamfortresslife@gmail.com>
2016-05-12 21:32:07 +0300

    test worker provides flow interface.

--- test/worker/worker.js
+++ test/worker/worker.js
@@ -6,7 +6,7 @@ var worker = module.exports = Worker()
 
 describe('worker', () =>
 {
-	it('handlers', () =>
+	it('have handlers', () =>
 	{
 		[
 			'init',
@@ -16,10 +16,22 @@ describe('worker', () =>
 			'down'
 		]
 		.forEach(it => expect(worker[it]).a('function'))
+	})
 
+	it('conn() provides Flow interface', () =>
+	{
 		worker.init = () =>
 		{
 			process.title = 'test-run-or-attach'
+		}
+
+		worker.conn = (flow) =>
+		{
+			expect(flow).an('object')
+
+			expect(flow.recv).a('function')
+			expect(flow.request).a('function')
+			expect(flow.socket).an('object')
 		}
 
 		return global.run
