311d165965953d2dda3e76257cf305a008dda90f
Strider <strange.transistor@gmail.com>
2017-11-16 16:22:14 +0300

    test: rm log, put delay.

--- test/worker/caller.js
+++ test/worker/caller.js
@@ -3,14 +3,17 @@ var sockpath = '/tmp/run-or-attach-worker-test'
 
 var attach = require('../../src/attach')
 
-var write = require('fs').createWriteStream
-var w = write(__dirname + '/../../output.txt')
+var delay = require('../_util').delay
+
+
+// var write = require('fs').createWriteStream
+// var w = write(__dirname + '/../../output.txt')
 // process.stdout.pipe(w)
 
 console.log   = (t) => { w.write('log:' + String(t) + '\n', 'utf-8') }
 console.error = (t) => { w.write('err:' + String(t) + '\n', 'utf-8') }
 
-console.log(module.parent && module.parent.filename)
+// console.log(module.parent && module.parent.filename)
 
 attach(sockpath)
 .then(flow =>
@@ -37,13 +40,18 @@ attach(sockpath)
 		return flow([ 'ok', 'data3' ])
 	})
 })
-.then(console.log, console.error)
-.then(() => console.log('OK'))
-.then(() =>
+// .then(console.log, console.error)
+// .then(() => console.log('OK'))
+/*.then(() =>
 {
 	return new Promise(rs =>
 	{
 		w.end(rs)
 	})
+})
+*/
+.then(() =>
+{
+	return delay(100)
 })
 .then(process.exit)
